19dc0364197663bcc9f5ab4c9f9df3859bd77cd7
LionsGold <leovankim@gmail.com>
2017-04-26 17:05:13 -0500

    SegmentController Fix
    Fixed the segment controller (that changes from list to collective
    view) problem.

---
+++ Bookmark/Bookmark/ListViewTableView.swift
@@ -0,0 +1,21 @@
+//
+//  ListViewTableView.swift
+//  Bookmark
+//
+//  Created by Leo Kim on 4/26/17.
+//  Copyright © 2017 Fink, Robert. All rights reserved.
+//
+
+import UIKit
+
+class ListViewTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Bookmark/Bookmark/ListViewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListViewTableViewCell.swift
+//  Bookmark
+//
+//  Created by Leo Kim on 4/26/17.
+//  Copyright © 2017 Fink, Robert. All rights reserved.
+//
+
+import UIKit
+
+class ListViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
