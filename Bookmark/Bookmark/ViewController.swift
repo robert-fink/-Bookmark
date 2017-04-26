566cc04124efdd95912735437ddff8374a96483e
rwfwcb <rwfwcb@mail.missouri.edu>
2017-04-25 20:13:28 -0500

    initial upload

---
+++ Bookmark/Bookmark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bookmark
+//
+//  Created by Admin on 4/23/17.
+//  Copyright © 2017 Fink, Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
