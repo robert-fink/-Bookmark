a30ad6ab77da9796f9ad3fcb8121cf5bc3322e2d
LionsGold <leovankim@gmail.com>
2017-04-26 12:52:07 -0500

    Load Screen & Profile View Controller
    Load Screen - Just a start (needs a logo still, and running into issue
    of the png not loading).
    
    Profile View Controller - added a view controller for Profile. (Need to
    figure out how to use the segmented control to interact with a
    UITableView, both collective and standard).

---
+++ Bookmark/Bookmark/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  Bookmark
+//
+//  Created by Leo Kim on 4/26/17.
+//  Copyright © 2017 Fink, Robert. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
