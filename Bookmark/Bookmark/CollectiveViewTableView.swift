0f59e885555db3ba0ea34ee353874cfe9eb7f623
LionsGold <leovankim@gmail.com>
2017-04-30 13:34:52 -0500

    Changes for 04/30/2017
    ~

---
+++ Bookmark/Bookmark/CollectiveViewTableView.swift
@@ -0,0 +1,21 @@
+//
+//  CollectiveViewTableView.swift
+//  Bookmark
+//
+//  Created by Leo Kim on 4/29/17.
+//  Copyright © 2017 Fink, Robert. All rights reserved.
+//
+
+import UIKit
+
+class CollectiveViewTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
